e8a9352ce7754c9d2ce114314da05691dbf53580
Kasem Wongsa <zero71st@gmail.com>
2018-10-20 10:41:52 +0700

    Initial ccommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace exxon_redeemtion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
